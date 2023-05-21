276afbcd17f31d4924e9e0ffc1a609953619b5f2
Jacob-Moore_lmig <Jacob.Moore@LibertyMutual.com>
2023-05-21 07:08:15 -0400

    css with counter

--- src/Counter.js
+++ src/Counter.js
@@ -1,11 +1,11 @@
 import React, {useState} from "react";
-
+import './Counter.css';
 function Counter(){
     const [count,setCount] = useState(0)
     const [mood,setMood] = useState(true)
     const togglemood =() =>setMood(!mood)
     return(
-        <div>
+        <div className="Counter">
             <h2>{count}</h2>
             <button onClick={() => setCount(count+1)}>+1</button>
             <h3 onClick={togglemood}>{mood ? ":)": ":("}</h3>
